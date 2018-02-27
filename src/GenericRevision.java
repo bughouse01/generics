554b2a6d3bdd1810a9d7b597b35eeeee5b03366e
Dave Kinmond <daviekinmond@gmail.com>
2018-02-27 20:24:14 +0000

    initial commit

--- src/GenericRevision.java
+++ src/GenericRevision.java
@@ -13,7 +13,7 @@ public class GenericRevision {
         assert s == 6;
 
         List<String> strings = Arrays.asList("Hello","World");
-        List<String> string2 = new ArrayList<String>();
+        List<String> string2 = new ArrayList<>();
 
         string2.add("Hello");
         string2.add("World");
